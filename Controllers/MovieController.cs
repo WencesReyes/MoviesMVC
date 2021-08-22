ce28e6ef35ef6714819de5c6ea279802edfc96cb
wenceslao reyes <wenceslao.reyes@msw.com.mx>
2021-08-22 18:56:15 -0500

    Adding movie controller

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using MoviesMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoviesMVC.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        [HttpGet]
+        public ActionResult Random()
+        {
+            var movie = new Movie { Id = 1, Name = "Shrek!" };
+            return View(movie);
+        }
+    }
+}
